79a737707f880984f6abc94e3d1f4baa6150a99f
otimbur <103147467+otimbur@users.noreply.github.com>
2023-02-12 22:16:49 -0800

    contact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import Nav from "../components/Nav/Nav";
 import type { AppProps } from "next/app";
 import React from "react";
-import { Footer } from "../components/Footer";
-import PersistentDrawerLeft from "../components/Nav/NavDrawer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
